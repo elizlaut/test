e2f97133be82dcc05b70069b4ed4b7b7f92c1e83
elizlaut <167748540+elizlaut@users.noreply.github.com>
2024-04-22 10:58:11 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+import requests
+import csv
+
+key = 'YY5A8WY7KTYS7Z86'
+
+CSV_URL = 'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=YY5A8WY7KTYS7Z86'
+
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+    decoded_content = download.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    for row in my_list:
+        print(row)
